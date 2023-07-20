1f1117909e1248fea03581d946d17521252071e6
Cabra <matiasleoni@gmail.com>
2023-07-20 12:11:55 -0300

    ch3 code docs

--- challenge3/diamond_pricing.py
+++ challenge3/diamond_pricing.py
@@ -3,16 +3,9 @@ from joblib import load
 import pandas as pd
 import numpy as np
 
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
-
-
-
 import os
 
+# Load trained model
 my_pipe = load('my_pipeline.joblib')
 
 if(os.name == 'posix'):
@@ -20,7 +13,7 @@ if(os.name == 'posix'):
 else:
    os.system('cls')
 
-
+# Input the Carat. Check if float.
 print('--------------------------------------------------------------------------------------------------')
 while True:
   a_carat = input('Introduce Carat of the Diamond (should be a number > 0) and press enter:')
@@ -30,6 +23,8 @@ while True:
   except ValueError:
     print('You must introduce a number. Please try again.')
     pass
+
+# Input the color. Check if value in value list.
 print('--------------------------------------------------------------------------------------------------')
 while True:
   a_color = input('Enter the Diamond Color (a letter from D to J) and press enter:')
@@ -37,6 +32,8 @@ while True:
     break
   else:
     print(f'{a_color} is not a correct value. Please try again.')
+
+# Input the clarity. Check if value in value list
 print('--------------------------------------------------------------------------------------------------')
 while True:
   print('Possible values of a Diamond Clarity are: I1, IF, SI1, SI2, VS1, VS2, VVS1, VVS2')
@@ -47,12 +44,16 @@ while True:
     print(f'{a_clarity} is not a correct value. Please try again.')
 
 
+# Drop the input data in a pandas dataframe to feed the Pipeline().predict method. 
 data = pd.DataFrame([[a_carat, a_color.upper(), a_clarity.upper()]], columns = ['carat', 'color', 'clarity'])
 
+# Obtain the price prediction
 a_price = np.exp(my_pipe.predict(data)[0])
 
+# prepare for the print
 to_format = (a_carat, a_color.upper(), a_clarity.upper(), a_price)
 
+# Output information
 print('--------------------------------------------------------------------------------------------------')
 print("The estimation for the value of a diamond with") 
 print("{0} carats, {1} color and {2} clarity is ${3:,.2f}".format(*to_format))
--- challenge3/pipeline_training.py
+++ challenge3/pipeline_training.py
@@ -9,43 +9,46 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 
 from joblib import dump
-
 import os
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-relative_path = os.path.join('datasets', 'diamonds','diamonds.csv')
-absolute_path = os.path.join(parent_dir, relative_path)
-
-data = pd.read_csv(absolute_path)
+# Load data and filter relevant columns
+data = pd.read_csv("../datasets/diamonds/diamonds.csv")
 data = data[['carat', 'color', 'clarity', 'price']]
 data = data[(data['carat']>0) & (data['price']>0)].reset_index(drop= True)
 
 
-#cath_columns = list(data.select_dtypes(include=[object]).columns)
-#num_columns = list(data.select_dtypes(exclude=[object]).columns)
-
+# We will use OneHotEncoder() to preprocess cathegorical variables and FunctionTransformer() with
+# a logarithm to preprocess the carat column. 
 cath_preprocessor = OneHotEncoder()
-num_preprocessor = FunctionTransformer(func = np.log, inverse_func = np.exp)#, feature_names_out = lambda x,y:(x,y))
+num_preprocessor = FunctionTransformer(func = np.log, inverse_func = np.exp)
+
+# Our simple model is a Linear Regression
 regressor = LinearRegression()
 
+# This combines the preprocessors and indicates the column to which they apply
 preprocessor = ColumnTransformer(
     transformers=[
         ('num_preprocessor', num_preprocessor, ['carat']),
         ('cath_preprocessor', cath_preprocessor, ['color', 'clarity'])
     ])
+
+# This defines the Pipeline object which combines the preprocess and the regression
 my_pipe = Pipeline([('transformaciones', preprocessor),('regresion', regressor)])
 
 
-
+# We separate the features and the target variable
 X = data.drop(['price'], axis = 1)
 y = np.log(data['price'])
 
+# We train the model
 my_pipe.fit(X,y)
 
+# We dump the trained model in a file for model persistence
+dump(my_pipe, 'my_pipeline.joblib')
 
 
 
+##### INFORMATION OUTPUT PRINTS
 if(os.name == 'posix'):
    os.system('clear')
 else:
@@ -56,9 +59,6 @@ print('The data Pipeline processed and fitted the model with the Diamond feature
 
 print("The coefficient of determination of the regression is  R^2 = {0:.3f}".format(my_pipe.score(X,y)))
 print('--------------------------------------------------------------------------------------------------')
-
-dump(my_pipe, 'my_pipeline.joblib') 
-
 print('')
 print('The data pipeline of the model was saved in the file "my_pipeline.joblib"')
 print('It can be loaded anytime to make a prediction.')
