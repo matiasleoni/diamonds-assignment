68149b1c5f220a0a7518316577bfb659b9c73331
Cabra <matiasleoni@gmail.com>
2023-07-27 09:17:10 -0300

    data control corrections

--- challenge4/src/app/main.py
+++ challenge4/src/app/main.py
@@ -33,7 +33,7 @@ def pricing(carat, color, clarity):
     API GET request to price a Diamond based on carat, color, and clarity.
     Parameters:
         carat: The carat weight of the diamond. It should be a positive number.
-        color: The color of the diamond. Should be a single uppercase letter from 'D' to 'J'.
+        color: The color of the diamond. Should be a single letter from 'D' to 'J'.
         clarity: The clarity of the diamond. Should be one of 'I1', 'IF', 'SI1', 'SI2', 'VS1', 'VS2', 'VVS1', or 'VVS2'.
     Returns:
         dict: A dictionary containing the following information:
@@ -43,20 +43,27 @@ def pricing(carat, color, clarity):
             - 'Price': The price of the diamond (float) if the input values are valid, or an error message (str) if any input is incorrect.
     """
     control = True
-    # Check if carat is float
+    # Check if carat is float and positive
     try:
         carat = float(carat)
+        if carat <= 0:
+            control = False
+            error_msg = 'Error: You must introduce a positive number for carat. Please try again.'    
     except ValueError:
         control = False
-        error_msg = 'You must introduce a number for carat. Please try again.'    
+        error_msg = 'Error: You must introduce a number for carat. Please try again.'    
     # Check color is in list
     if color.upper() not in list('DEFGHIJ'):
         control = False
-        error_msg = f'{color} is not a correct value for color. Please use a letter from D to J.'
+        error_msg = f'Error: {color} is not a correct value for color. Please use a letter from D to J.'
+    else:
+        color = color.upper()
     # Check clarity is in list
     if clarity.upper() not in ["I1", "IF", "SI1", "SI2", "VS1", "VS2", "VVS1", "VVS2"]:
         control = False
-        error_msg = f'{clarity} is not a correct value for clarity. Please use I1, IF, SI1, SI2, VS1, VS2, VVS1, VVS2.'
+        error_msg = f'Error: {clarity} is not a correct value for clarity. Please use I1, IF, SI1, SI2, VS1, VS2, VVS1, VVS2.'
+    else:
+        clarity = clarity.upper()
 
     # If all checks passed, predict and present price (with input info), else show error.
     if control:
